5efd839fd4aa976374927cea13e2dd2626dc1710
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-01-30 15:13:24 +0300

    Update main.py & README.md

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # gladiators
 
-A simple game modeling fighting between two teams of gladiators.
-Red and Blue team fight each other, if gladiator win, he grub all the power of the enemy.
-If the Power of gladiators is equal both of them lost.
+A simple game modeling fighting between two teams of gladiators.  
+Red and Blue team fight each other, if the gladiator win, it will grub all the Power  of the enemy.  
+If the Power of gladiators is equal both of them lost.  
 
-The team with the most survived gladiators wins. If lost results are equal, the Draw will be announced.
+The team with the most survived gladiators wins.  
+If lost results are equal, the Draw will be announced.  
--- main.py
+++ main.py
@@ -1,22 +1,37 @@
+import time
 import random
 
-# Create commands randomly
+# Create teams with random power
 
-red_team = [random.randint(50, 100) for r in range(6)]
-red_team.insert(0, 'R')
-print('The RED team at the beginning', red_team)
+red_team = []
+blue_team = []
+teams_members = 6
 
-blue_team = [random.randint(50, 100) for b in range(6)]
-blue_team.insert(0, 'B')
-print('The BLUE team at the beginning', blue_team)
+def create_teams(teams_members = teams_members):
+    if teams_members <= 0:
+        print('Nothing to think about. Everyone sleep')
+        exit()
+    else:
+        global red_team
+        red_team = [random.randint(50, 100) for r in range(teams_members)]
+        red_team.insert(0, 'R')
+        print('The RED team at the beginning', red_team)
+
+        global blue_team
+        blue_team = [random.randint(50, 100) for b in range(teams_members)]
+        blue_team.insert(0, 'B')
+        print('The BLUE team at the beginning', blue_team)
 
 # Make them fight
 
 def fighting(team_1=[], team_2=[]):
+
     print('OPPONENT TABLE')
-    for i in range(1,len(team_1)):
+    for i in range(1, len(team_1)):
         print(team_1[i], end=' ')
         print(team_2[i])
+
+        time.sleep(0.5)
 
         if team_1[i] > team_2[i]:
             team_1[i] += team_2[i]
@@ -30,44 +45,71 @@ def fighting(team_1=[], team_2=[]):
 
 # Gather results
 
-def result(team_1=[], team_2=[]):
+def result(team_1=[], team_2=[], teams_members = teams_members):
 
     results = 'FINAL SCORE'
     print('*' * len(results))
     print(results)
     print('*' * len(results))
 
+    over_kill_message = '!!! TOTAL ANNIHILATION !!!'
+
+    time.sleep(2)
+
     if team_1[0] == 'R' and team_2[0] == 'B':
-        print('THE RED TEAM', team_1)
-        print('THE BLUE TEAM', team_2)
+        print('The RED team after fight', team_1)
+        print('The BLUE team after fight', team_2)
 
         res_1 = team_1.count(0)
-        print('RED', res_1)
         res_2 = team_2.count(0)
-        print('BLUE', res_2)
+
+        time.sleep(1)
+
+        print('The RED team lost', res_1, 'gladiators')
+        print('The BLUE team lost', res_2, 'gladiators')
 
         if res_1 > res_2:
             print('BLUE TEAM WIN !')
+            if res_2 == 0 and res_1 == teams_members:
+                print('*' * len(over_kill_message))
+                print(over_kill_message)
+                print('*' * len(over_kill_message))
         elif res_1 < res_2:
             print('RED TEAM WIN !')
+            if res_1 == 0 and res_2 == teams_members:
+                print('*' * len(over_kill_message))
+                print(over_kill_message)
+                print('*' * len(over_kill_message))
         else:
             print('DRAW !')
 
     else:
-        print('THE RED TEAM', team_2)
-        print('THE BLUE TEAM', team_1)
+        print('The RED team after fight', team_2)
+        print('The BLUE team after fight', team_1)
 
         res_2 = team_2.count(0)
-        print('RED TEAM GLADIATORS LOST', res_2)
         res_1 = team_1.count(0)
-        print('BLUE TEAM GLADIATORS LOST', res_1)
+
+        time.sleep(1)
+
+        print('The RED team lost', res_2, 'gladiators')
+        print('The BLUE team lost', res_1, 'gladiators')
 
         if res_1 > res_2:
             print('RED TEAM WIN !')
+            if res_2 == 0 and res_1 == teams_members:
+                print('*' * len(over_kill_message))
+                print(over_kill_message)
+                print('*' * len(over_kill_message))
         elif res_1 < res_2:
             print('BLUE TEAM WIN !')
+            if res_1 == 0 and res_2 == teams_members:
+                print('*' * len(over_kill_message))
+                print(over_kill_message)
+                print('*' * len(over_kill_message))
         else:
             print('DRAW !')
 
-fighting(red_team, blue_team)
-result(blue_team, red_team)
+create_teams(teams_members)
+fighting(blue_team, red_team)
+result(red_team, blue_team, teams_members)
