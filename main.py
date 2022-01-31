b9f9eb69b5726e67a733d89f610ee1f190e84623
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-01-31 10:59:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,27 +3,23 @@ import random
 
 # Create teams with random power
 
-red_team = []
-blue_team = []
+flag_team1 = 'BLACK'
+flag_team2 = 'ORANGE'
 teams_members = 6
 
-def create_teams(teams_members = teams_members):
+def create_teams(flag_team, teams_members = teams_members):
     if teams_members <= 0:
         print('Nothing to think about. Everyone sleep')
         exit()
     else:
-        global red_team
-        red_team = [random.randint(50, 100) for r in range(teams_members)]
-        red_team.insert(0, 'R')
-        print('The RED team at the beginning', red_team)
+        team = [random.randint(50, 100) for r in range(teams_members)]
+        team.insert(0, flag_team)
+        print('The', flag_team, 'team at the beginning', team)
 
-        global blue_team
-        blue_team = [random.randint(50, 100) for b in range(teams_members)]
-        blue_team.insert(0, 'B')
-        print('The BLUE team at the beginning', blue_team)
+        return team
+
 
 # Make them fight
-
 def fighting(team_1=[], team_2=[]):
 
     print('OPPONENT TABLE')
@@ -43,9 +39,9 @@ def fighting(team_1=[], team_2=[]):
             team_1[i] = 0
             team_2[i] = 0
 
-# Gather results
 
-def result(team_1=[], team_2=[], teams_members = teams_members):
+# Gather results
+def result(team_1=[], team_2=[], teams_members = teams_members, flag_team1 = flag_team1, flag_team2 = flag_team2):
 
     results = 'FINAL SCORE'
     print('*' * len(results))
@@ -56,26 +52,26 @@ def result(team_1=[], team_2=[], teams_members = teams_members):
 
     time.sleep(2)
 
-    if team_1[0] == 'R' and team_2[0] == 'B':
-        print('The RED team after fight', team_1)
-        print('The BLUE team after fight', team_2)
+    if team_1[0] == flag_team1 and team_2[0] == flag_team2:
+        print('The', flag_team1, 'team after fight', team_1)
+        print('The', flag_team2, 'team after fight', team_2)
 
         res_1 = team_1.count(0)
         res_2 = team_2.count(0)
 
         time.sleep(1)
 
-        print('The RED team lost', res_1, 'gladiators')
-        print('The BLUE team lost', res_2, 'gladiators')
+        print('The', flag_team1, 'team lost', res_1, 'gladiators')
+        print('The', flag_team2, 'team lost', res_2, 'gladiators')
 
         if res_1 > res_2:
-            print('BLUE TEAM WIN !')
+            print(flag_team2, 'TEAM WIN !')
             if res_2 == 0 and res_1 == teams_members:
                 print('*' * len(over_kill_message))
                 print(over_kill_message)
                 print('*' * len(over_kill_message))
         elif res_1 < res_2:
-            print('RED TEAM WIN !')
+            print(flag_team1, 'TEAM WIN !')
             if res_1 == 0 and res_2 == teams_members:
                 print('*' * len(over_kill_message))
                 print(over_kill_message)
@@ -84,25 +80,25 @@ def result(team_1=[], team_2=[], teams_members = teams_members):
             print('DRAW !')
 
     else:
-        print('The RED team after fight', team_2)
-        print('The BLUE team after fight', team_1)
+        print('The', flag_team2, 'team after fight', team_2)
+        print('The', flag_team1, 'team after fight', team_1)
 
         res_2 = team_2.count(0)
         res_1 = team_1.count(0)
 
         time.sleep(1)
 
-        print('The RED team lost', res_2, 'gladiators')
-        print('The BLUE team lost', res_1, 'gladiators')
+        print('The', flag_team2, 'team lost', res_2, 'gladiators')
+        print('The', flag_team1, 'team lost', res_1, 'gladiators')
 
         if res_1 > res_2:
-            print('RED TEAM WIN !')
+            print(flag_team2, 'TEAM WIN !')
             if res_2 == 0 and res_1 == teams_members:
                 print('*' * len(over_kill_message))
                 print(over_kill_message)
                 print('*' * len(over_kill_message))
         elif res_1 < res_2:
-            print('BLUE TEAM WIN !')
+            print(flag_team1, 'TEAM WIN !')
             if res_1 == 0 and res_2 == teams_members:
                 print('*' * len(over_kill_message))
                 print(over_kill_message)
@@ -110,6 +106,9 @@ def result(team_1=[], team_2=[], teams_members = teams_members):
         else:
             print('DRAW !')
 
-create_teams(teams_members)
-fighting(blue_team, red_team)
-result(red_team, blue_team, teams_members)
+
+team_1 = create_teams(flag_team1, teams_members)
+team_2 = create_teams(flag_team2, teams_members)
+
+fighting(team_1, team_2)
+result(team_1, team_2, teams_members, flag_team1, flag_team2)
